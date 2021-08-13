1baf4d8d76333c50138263a181a2311a28f5e03a
Steve Clark <steve.a.clark48@gmail.com>
2021-08-13 10:56:13 -0500

    setup
    setup up axios and cors

---
+++ musiclibrary/src/components/App.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {}
+    }
+
+    render() {
+        return(
+            <h1>Paul is CUTE!! Not...</h1>
+        )
+    }
+}
+
+export default App;
