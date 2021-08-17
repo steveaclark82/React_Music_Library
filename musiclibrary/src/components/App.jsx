b9effc77087f7f177ab05d9ff8fd8d036094c021
kschao <koyschao@icloud.com>
2021-08-17 12:01:15 -0500

    fixed music table spelling

--- musiclibrary/src/components/App.jsx
+++ musiclibrary/src/components/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component} from 'react';
-import Musictable from './musicTable/MusicTable'
+import Musictable from './musicTable/Musictable';
 import axios from 'axios';
 import SearchBar from './SearchBar/SearchBar.css';
 import CreateSong from './CreateSong/createSong';
